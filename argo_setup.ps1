9fe1e7f77e00b3e8100ab81a516fb282a4c43aee
Svabas22 <simonas.svabas2002@gmail.com>
2025-02-04 16:39:27 +0200

    patch

--- argo_setup.ps1
+++ argo_setup.ps1
@@ -3,6 +3,8 @@ kubectl create ns argocd
 
 helm install argo-cd argo-cd/argo-cd -n argocd -f values.yaml
 
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl get secret grafana -n argocd -o jsonpath="{.data.admin-password}")))
+
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}")))
 #the command looks scary due to how decoding Base64 works in PowerShell without extra utilities. Bash example:
 #kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode
